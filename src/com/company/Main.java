51fdb2b10e357ad4177d3fcd9b86dce1facfc257
UtkinVladimirV <68727544+UtkinVladimirV@users.noreply.github.com>
2020-08-01 23:49:05 +0300

    задача №1 тест выгрузки

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int a=10;
+        int b=7;
+        int z;
+
+      System.out.println(z=((a-3)*b/2));
+
+
+
+
+
+
+
+
+    }
+}
